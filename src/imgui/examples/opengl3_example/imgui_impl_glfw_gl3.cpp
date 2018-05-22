48ad9be7de9b4c5fdaa4eb9455adae262bf53280
Tobias Markus <tobias@miglix.eu>
2018-05-22 12:17:58 +0200

    imgui: Fix include paths
    As a precaution, fix ImGui's include paths (use angle brackets).

--- src/imgui/examples/opengl3_example/imgui_impl_glfw_gl3.cpp
+++ src/imgui/examples/opengl3_example/imgui_impl_glfw_gl3.cpp
@@ -37,8 +37,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #endif
 
-#include "imgui.h"
-#include "imgui_impl_glfw_gl3.h"
+#include <imgui.h>
+#include <imgui_impl_glfw_gl3.h>
 
 // GL3W/GLFW
 //#include <GL/gl3w.h>    // This example is using gl3w to access OpenGL functions (because it is small). You may use glew/glad/glLoadGen/etc. whatever already works for you.
